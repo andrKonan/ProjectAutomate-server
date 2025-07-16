3e679f8ce6a27b7e78f6504296dad30dac5c2704
andrKonan <52403327+andrKonan@users.noreply.github.com>
2025-07-16 14:16:08 +0200

    Add basic items and clients model

---
+++ models/clients.py
@@ -0,0 +1,14 @@
+# server/models/clients.py
+from __future__ import annotations
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from models.base import BaseRepr
+
+
+class Client(BaseRepr):
+    __tablename__ = "clients"
+
+    name: Mapped[str]       = mapped_column(String,  unique=True)
+    _token: Mapped[str]     = mapped_column(String,  unique=True, index=True)
---
+++ models/items.py
@@ -0,0 +1,14 @@
+# server/models/items.py
+from __future__ import annotations
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from models.base import BaseRepr
+
+
+class ItemType(BaseRepr):
+    __tablename__ = "item_types"
+
+    name: Mapped[str] = mapped_column(String, unique=True)
+    durability: Mapped[int | None] = mapped_column(Integer, nullable=True, default=None)
