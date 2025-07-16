9f3d7cd8adab64f04ebb5ffd3b9af48c48781e0c
andrKonan <52403327+andrKonan@users.noreply.github.com>
2025-07-16 14:18:07 +0200

    Add items and clients models to __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,10 @@
 # server/models/__init__.py
 from models.base import BaseRepr
+from models.clients import Client
+from models.items import ItemType
 
 __all__ = [
-    "BaseRepr"
+    "BaseRepr",
+    "Client",
+    "ItemType"
 ]
